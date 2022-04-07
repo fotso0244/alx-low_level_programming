a389f6bb56f624adb9cee49525a949cb7f83c1bf
aristide notouom <fotso0244@gmail.com>
2022-04-07 15:21:30 -0700

    How to print String with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-/*prints strings in stdout with puts function*/
-
 #include <stdio.h>
 
 int main(void)
-{
+{	/*Print string with puts function*/
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
