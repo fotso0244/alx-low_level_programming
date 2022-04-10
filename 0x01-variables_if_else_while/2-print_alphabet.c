b33bf3ee17312b4fb5d995c1e70ed56327ec78cc
aristide notouom <fotso0244@gmail.com>
2022-04-10 09:18:48 -0700

    How to print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char car;
+
+	for (car = 'a'; car <= 'z'; car++)
+	{
+		putchar(car);
+	}
+	printf("\n");
+	return (0);
+}
