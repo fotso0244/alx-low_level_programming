3190b3ced41dd4e7245b7e0db2a08b324691f3ab
aristide notouom <fotso0244@gmail.com>
2022-04-12 15:47:43 -0700

    How to print _putchar without printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	char str[8] = "_putchar";
+	int i;
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return(0);
+}
