60d2c753f2dba6b6ca60d35be2e63a4a7eb505e2
aristide notouom <fotso0244@gmail.com>
2022-04-12 17:09:56 -0700

    How to check sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ * @n: is a number to be checked
+ *
+ * Return: 1 if n gt 0, 0 if n==0, -1 if n lt 0
+ */
+int print_sign(int n)
+{
+	int s;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		s = 1;
+	}
+	else
+		if (n < 0)
+		{
+			_putchar('-');
+			s = -1;
+		}
+		else
+		{
+			_putchar('0');
+			s = 0;
+		}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ Function 1-alphabet.c prints alphabets in lowercase using _putchar
 Function 2-print_alphabet_x10.c prints alphabet in lowercase 10 times using _putchar
 Function 3-islower checks lowercase character
 Function 4-isalpha.c checks for alphabetic character
+Function 5-sign.c checks sign of number
