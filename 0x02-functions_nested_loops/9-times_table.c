c223756d7dfd49e99e83d0427a045c50962db4d4
aristide notouom <fotso0244@gmail.com>
2022-04-12 18:38:00 -0700

    How to print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,8 +11,8 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			if (((i * j) / 10) != 0)
-				_putchar((i * j) / 10) + '0');
-		_putchar((i * j) % 10) + '0');
+				_putchar(((i * j) / 10) + '0');
+		_putchar(((i * j) % 10) + '0');
 		_putchar(',');
 		_putchar(' ');
 		}
