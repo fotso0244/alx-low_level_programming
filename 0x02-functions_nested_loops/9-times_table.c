cc2c0aff2b6913906a8379abc2af65785606f2e2
aristide notouom <fotso0244@gmail.com>
2022-04-12 19:36:46 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,10 +17,12 @@ void times_table(void)
 				{
 					_putchar(' ');
 				}
-		_putchar(((i * j) % 10) + '0');
-		if (j != 9)
-			_putchar(',');
-		_putchar(' ');
+			_putchar(((i * j) % 10) + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
