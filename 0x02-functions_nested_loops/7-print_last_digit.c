e9bc97ecc83037f8c249fbd711f45ce199749cab
aristide notouom <fotso0244@gmail.com>
2022-04-12 17:29:29 -0700

    How to print last digit of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of an integer
+ * @n: The number to print last digit
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int s;
+
+	if (n >= 0)
+		s = n % 10;
+	else
+		s = (n * (-1)) %  10;
+	return (s);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -5,3 +5,4 @@ Function 3-islower checks lowercase character
 Function 4-isalpha.c checks for alphabetic character
 Function 5-sign.c checks sign of number
 Function 6-abs.c returns absolute value of an integer
+Function 7-print_last_digit prints last digit of an integer
