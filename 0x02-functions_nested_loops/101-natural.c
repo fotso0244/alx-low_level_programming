a82a898830653addad0d8a28b0a95d82faa3e030
aristide notouom <fotso0244@gmail.com>
2022-04-13 03:01:08 -0700

    How to print sum of multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * print_sum_multiples - prints sum of multiples of 3 and 5
- * @n: integer to find multiples below
+ * main - prints sum of multiples of 3 and 5
  *
- * Return: sum of multiples of 3 and 5
+ * Return: 0
  */
-void _start(void)
+int main(void)
 {
 	int sum = 0, i;
 
 	for (i = 1; i < 1024; i++)
-		{
-			if (((i % 3) == 0) || ((i % 5) == 0))
-				sum = sum + i;
-		}
-		printf("%d", sum);
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+			sum = sum + i;
+	}
+	printf("%d", sum);
+	return (0)
 }
