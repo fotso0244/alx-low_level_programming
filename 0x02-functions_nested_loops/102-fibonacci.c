b86a2aee8b8c753f204ef3b40dce669eb5165ac6
aristide notouom <fotso0244@gmail.com>
2022-04-13 08:47:42 -0700

    How to print 50 numbers fibonacci beginning by 1 and 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,9 @@ int fibonacci(int n)
 {
 	int res;
 
-	if (n == 1)
-		res = 1;
-	else
-		if (n == 0)
-			res = 0;
-		else
-			res = fibonacci(n - 1) + fibonacci(n - 2);
-	return (res);
+	if (n <= 1)
+		return (n);
+	return (fibonacci(n - 1) + fibonacci(n - 2));
 }
 int main(void)
 {
@@ -27,7 +22,6 @@ int main(void)
 		printf("%d", fibonacci(i));
 		if (i != 50)
 			printf(", ");
-		printf("gfghfg");
 	}
 	return (0);
 }
