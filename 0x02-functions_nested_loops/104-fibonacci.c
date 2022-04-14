6eaafbbed651e1845fe38243d771cab94a3eea1e
aristide notouom <fotso0244@gmail.com>
2022-04-13 18:34:14 -0700

    How tp print 98 first fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * fibonacci - computes 50 first fibonacci numbers
+ * @n: fibonacci number
+ *
+ * Return: fibonacci(n)
+ */
+int fibonacci(int n)
+{
+
+	if (n <= 1)
+		return (n);
+	return (fibonacci(n - 1) + fibonacci(n - 2));
+}
+/**
+ * main - computes fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 98; i++)
+	{
+		printf("%d", fibonacci(i));
+		if (i != 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
