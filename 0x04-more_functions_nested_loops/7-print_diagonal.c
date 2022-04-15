f0608a88f21eb8ac48810c04ea2f702b7831a193
aristide notouom <fotso0244@gmail.com>
2022-04-14 20:50:35 -0700

    How to print \ n times

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,14 +5,20 @@
  */
 void print_diagonal(int n)
 {
-	int j;
+	int j, k;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 		for (j = 0; j <= n - 1; j++)
 		{
+			if (n != 1)
+			{
+				for (k = 1; k <= j; k++)
+					_putchar(' ');
+			}
 			_putchar(92);
+			_putchar('\n');
 			if (j == n - 1)
 				_putchar('\n');
 		}
