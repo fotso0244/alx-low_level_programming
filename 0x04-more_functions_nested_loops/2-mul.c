617f94c95b3fe720b7501c40811e45440bda0f3c
aristide notouom <fotso0244@gmail.com>
2022-04-14 18:53:44 -0700

    How to multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: multiplication of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 Code 0-isupper.c checks for uppercase character
 Code 1-isdigit.c checks for digit
+Code 2-mul.c returns multiplication of 2 integers
