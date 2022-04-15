922a5c820fc1b2c3e0b175f9fc895fcd57de7743
aristide notouom <fotso0244@gmail.com>
2022-04-14 20:00:58 -0700

    How to print _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints charcter _ n times
+ * @n: number of times
+ */
+void print_line(n)
+{
+	int i = n;
+
+	if (i <= 0)
+		_putchar('\n');
+	else
+		while (i > 0)
+		{
+			_putchar('_');
+			i--;
+		}
+}
+
