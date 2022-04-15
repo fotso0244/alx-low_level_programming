3b3a6fec7aad9220cabd5049e07df71f821710cf
aristide notouom <fotso0244@gmail.com>
2022-04-15 01:51:42 -0700

    update code 10-

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,9 +9,10 @@
 int puiss(int a, int b)
 {
 	int i, res = 1;
+
 	for (i = 1; i <= b; i++)
 		res *= a;
-	return res;
+	return (res);
 }
 /**
  * print_number - print an integer
@@ -23,9 +24,8 @@ void print_number(int n)
 	if (a < 0)
 	{
 		_putchar('-');
-		a*=(-1);
+		a *= (-1);
 	}
-
 	while ((a / 10 >= 10) || (a / 10 < 10))
 	{
 		if (a / 10 >= 10)
@@ -43,7 +43,7 @@ void print_number(int n)
 				break;
 			if (b >= 1)
 			{
-				a = c % (puiss(10,b));
+				a = c % (puiss(10, b));
 				c = a;
 				b = 0;
 			}
