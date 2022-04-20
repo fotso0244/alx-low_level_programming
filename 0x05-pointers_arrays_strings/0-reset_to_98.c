45e2001edbb8380f5c84b159f07b439a9016fc7a
aristide notouom <fotso0244@gmail.com>
2022-04-20 16:02:18 -0700

    How to change valute of int pointer in function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * reset_to_98 - change value of parameter to 98
+ * @*n: pointer to change value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Code 0-reset_to_98.c change value of integer pointer to 98
