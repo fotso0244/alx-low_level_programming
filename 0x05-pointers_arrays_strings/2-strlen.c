dbfc42eb758b00804829227f2ca407e2d63aba6b
aristide notouom <fotso0244@gmail.com>
2022-04-20 16:18:20 -0700

    How to return length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strlen - returns length of a string
+ * @s: string to return length
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 Code 0-reset_to_98.c change value of integer pointer to 98
 Code 1-swap.c swaps 2 int pointers
+Code 2-strlen.c returns length of a string
