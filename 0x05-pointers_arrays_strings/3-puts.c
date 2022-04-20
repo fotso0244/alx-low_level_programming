fbc822784f88cb94fe1b62af7151b41a8204eae5
aristide notouom <fotso0244@gmail.com>
2022-04-20 16:34:07 -0700

    How to print a string using _putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a tring with _putchar function
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < (int)strlen(str) - 1; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 Code 0-reset_to_98.c change value of integer pointer to 98
 Code 1-swap.c swaps 2 int pointers
 Code 2-strlen.c returns length of a string
+Code 3-puts.c prints a string using _putchar
