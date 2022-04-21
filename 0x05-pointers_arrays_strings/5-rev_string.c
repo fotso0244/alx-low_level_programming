41b8a9710471227e23b7d336b98e589c2d7dd151
aristide notouom <fotso0244@gmail.com>
2022-04-20 17:24:13 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,16 +6,12 @@
  */
 void rev_string(char *s)
 {
-	int i;
+	int i = strlen(s) - 1;
+	char *revstr = " ";
 
-	for (i = 0; i <= (int)strlen(s) -1; i++)
-		_putchar(s[i]);
-	_putchar('\n');
-	i = (int)strlen(s) - 1;
 	while (i >= 0)
 	{
-		_putchar(s[i]);
+		strcat(revstr,s[i]);
 		i--;
 	}
-	_putchar('\n');
 }
