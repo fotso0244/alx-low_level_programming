67587d448fe1f693f171ed29b9c27dabf3bf9b80
aristide notouom <fotso0244@gmail.com>
2022-04-20 18:53:01 -0700

    How to print other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,12 @@ void puts2(char *str)
 	int i = 0;
 
 	_putchar(str[i]);
-	while (i <= (int)strlen(str) - 3)
-	{
-		i = i + 2;
-		_putchar(str[i]);
-	}
+	if ((int)strlen(str) >= 3)
+		while (i <= (int)strlen(str) - 3)
+		{
+			i = i + 2;
+			_putchar(str[i]);
+		}
 	_putchar('\n');
 }
 
