40401aaf42957e38d0834b1f4de638fba76d1e92
aristide notouom <fotso0244@gmail.com>
2022-04-25 12:51:32 -0700

    How to change lowercase letters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
  * string_toupper - changes lowercase letters to uppercase
  * @s: string
@@ -13,7 +14,7 @@ char *string_toupper(char *s)
 	for (j = 0; j <= (int)strlen(s) - 1; j++)
 	{
 		if (*(s + j) <= 122 && *(s + j) >= 97)
-			*(s + j) += 32;
+			*(s + j) -= 32;
 	}
 	return (s);
 }
