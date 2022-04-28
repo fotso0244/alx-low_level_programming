71fa7d2709f16e74e1f30856b7412d607dc13ea8
aristide notouom <fotso0244@gmail.com>
2022-04-28 15:58:45 -0700

    Update 104..

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,7 +12,7 @@ void printspace(int size, int i, int j)
 	int nbspace, k;
 
 	if ((size % 10) % 2 == 0 && size > 10)
-		if ((size % 10 != 0) && (i == (size / 10) * 10) && (j == (size / 10) * 10))
+		if ((size % 10 != 0) && (i == (size / 10) * 10) && j == i)
 		{
 			nbspace = ((10 - (size % 10)) * 2) + ((10 - (size % 10)) / 2);
 				for (k = 0; k <= nbspace - 1; k++)
@@ -20,12 +20,24 @@ void printspace(int size, int i, int j)
 		}
 
 	if ((size % 10) % 2 != 0 && size > 10)
-		if ((size % 10 != 0) && (i == (size / 10) * 10) && (j == (size / 10) * 10))
+		if ((size % 10 != 0) && (i == (size / 10) * 10) && j == i)
 		{
 			nbspace = ((10 - (size % 10)) * 2) + ((10 - (size % 10)) / 2) + 1;
 				for (k = 0; k <= nbspace - 1; k++)
 					printf(" ");
 		}
+}
+/**
+ * print - print character
+ * @c: character
+ */
+void print(char c)
+{
+
+	if (isprint(c) != 0)
+		printf("%x", c);
+	else
+		printf("0%x", c);
 }
 /**
  * print_buffer - print size bytes of buffer
@@ -37,13 +49,15 @@ void print_buffer(char *b, int size)
 	int i = 0, j = 0;
 
 	if (size > 0)
-		while (i < (size - 1))
+		while (i <= (size - 1))
 		{
 			j = i;
 			if (i <= 10)
 				printf("0000000%x: ", i);
 			else
+			{
 				printf("000000%x: ", i);
+			}
 			while ((j <= i + 8) && (j <= size - 2))
 			{
 				if (isprint(b[j]) != 0)
@@ -56,8 +70,8 @@ void print_buffer(char *b, int size)
 					printf("0%x ", b[j + 1]);
 				j += 2;
 			}
-			if (size % 2 != 0 && j == size - 1)
-				printf("%x ", b[size - 1]);
+			if (size % 2 != 0 && j == size - 1 && (j - i) < 10 && size > 10)
+				print(b[j]);
 			j = i;
 			while ((j <= i + 9) && (j <= size - 1))
 			{
