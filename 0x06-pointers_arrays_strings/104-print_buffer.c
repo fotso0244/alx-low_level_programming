bf8add8af34b7f0bf610b3b79c0f1e7ad63b0c2f
aristide notouom <fotso0244@gmail.com>
2022-04-27 17:01:59 -0700

    How to print n bytes of buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,30 @@
+#include <ctype.h>
+/**
+ * print_buffer - print size bytes of buffer
+ * @b: buffer
+ * @size: number of bytes to print
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j = 0;
+
+	while (i <= size - 1)
+	{
+		printf("0000000%x: ", i);
+		while (j <= 8)
+		{
+			printf("%x%x ", b[j], b[j + 1]);
+			j += 2;
+		}
+		j = 0;
+		while (j <= 9)
+		{
+			if (isprint(b[j]) != 0)
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+		printf("\n");
+		i += 10;
+	}
+}
