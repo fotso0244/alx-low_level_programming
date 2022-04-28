7bdfe5ffc848c428fb853017bd92e88c3862379c
aristide notouom <fotso0244@gmail.com>
2022-04-27 19:46:28 -0700

    update 104..

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,13 +2,39 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * printspace - prints spaces
+ * @size: int
+ * @i: int
+ * @j: int
+ */
+void printspace(int size, int i, int j)
+{
+	int nbspace, k;
+
+	if ((size % 10) % 2 == 0)
+		if ((size % 10 != 0) && (i == (size / 10) * 10) && (j == (size / 10) * 10))
+		{
+			nbspace = ((10 - (size % 10)) * 2) + ((10 - (size % 10)) / 2);
+				for (k = 0; k <= nbspace - 1; k++)
+					printf(" ");
+		}
+
+	if ((size % 10) % 2 != 0)
+		if ((size % 10 != 0) && (i == (size / 10) * 10) && (j == (size / 10) * 10))
+		{
+			nbspace = ((10 - (size % 10)) * 2) + ((10 - (size % 10)) / 2) + 3;
+				for (k = 0; k <= nbspace - 1; k++)
+					printf(" ");
+		}
+}
+/**
  * print_buffer - print size bytes of buffer
  * @b: buffer
  * @size: number of bytes to print
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j = 0, nbspace, k;
+	int i = 0, j = 0;
 
 	if (size > 0)
 		while (i < (size - 1))
@@ -27,15 +53,12 @@ void print_buffer(char *b, int size)
 					printf("0%x ", b[j + 1]);
 				j += 2;
 			}
+			if (size % 2 != 0)
+				printf("%x", b[size - 1]);
 			j = i;
 			while ((j <= i + 9) && (j <= size - 1))
 			{
-				if ((size % 10 != 0) && (i == (size / 10) * 10) && (j == (size / 10) * 10))
-				{
-					nbspace = ((10 - (size % 10)) * 2) + 2;
-					for (k = 0; k <= nbspace - 1; k++)
-						printf(" ");
-				}
+				printspace(size, i, j);
 				if (isprint(b[j]) != 0)
 					printf("%c", b[j]);
 				else
