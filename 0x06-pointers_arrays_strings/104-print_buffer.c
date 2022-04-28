0d370a81eead415deed983bf7e54be3a7773e72f
aristide notouom <fotso0244@gmail.com>
2022-04-27 18:47:47 -0700

    ipdate 104..

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,12 +8,14 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, nbspace, k;
 
-	while (i <= (size - 1))
+	printf("%d\n", size);
+	printf("%d\n", (int)strlen(b));
+	while (i < (size - 1))
 	{
 		printf("0000000%x: ", i);
-		while (j <= i + 8)
+		while ((j <= i + 8) && (j <= size - 2))
 		{
 			if (isprint(b[j]) != 0)
 				printf("%x", b[j]);
@@ -26,8 +28,14 @@ void print_buffer(char *b, int size)
 			j += 2;
 		}
 		j = i;
-		while (j <= i + 9)
+		while ((j <= i + 9) && (j <= size - 1))
 		{
+			if ((size % 10 != 0) && (i == (size / 10) * 10) && (j == (size / 10) * 10))
+			{
+				nbspace = ((10 - (size % 10)) * 2) + 2;
+				for (k = 0; k <= nbspace - 1; k++)
+					printf(" ");
+			}
 			if (isprint(b[j]) != 0)
 				printf("%c", b[j]);
 			else
