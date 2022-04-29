0e24f3a123aaea153c804afe21779234d88dc9ac
aristide notouom <fotso0244@gmail.com>
2022-04-29 16:55:06 -0700

    How to print length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - computes length of a string
+ * @s: string
+ *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
