2625246c1008235ef8d88fbbc859af4ad24e14b9
aristide notouom <fotso0244@gmail.com>
2022-04-29 17:14:21 -0700

    How to compute x raised a power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * pow_recursion - computes x raised to the power of y
+ * @x: base
+ * @y: power
+ *
+ * Return: x raised power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return 1;
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,4 @@ Code 0-puts-recursion prints a string
 Code 1-print_rev_recursion.c prints a string in reverse
 Code 2-strlen_recursion.c prints length of a string
 Code 3-factorial.c prints factorial of an integer
+Code 4-pow_recursion.c prints x raised a power of y
