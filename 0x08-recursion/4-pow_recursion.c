78d49dabd38ea7712d20a9b49e8d6c1989917790
aristide notouom <fotso0244@gmail.com>
2022-04-29 17:18:02 -0700

    How to compute x raised a power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,5 @@
 /**
- * pow_recursion - computes x raised to the power of y
+ * _pow_recursion - computes x raised to the power of y
  * @x: base
  * @y: power
  *
@@ -8,7 +8,7 @@
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
-		return 1;
+		return (1);
 	if (y < 0)
 		return (-1);
 	return (x * _pow_recursion(x, y - 1));
