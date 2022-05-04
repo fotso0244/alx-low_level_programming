2294d66d5be69f55bb2a75b025a4ff98ee49d8e1
aristide notouom <fotso0244@gmail.com>
2022-05-03 17:24:45 -0700

    How to search a string for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to scan
+ * @accept: set of bytes
+ *
+ * Return: pointer to the byte in s that matches of the bytes in accept
+ */char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j = 0;
+	char *res;
+	
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				res = s + i;
+				break;
+			}
+			j++;
+		}
+		if(accept[j] != '\0')
+			break;
+		j = 0;
+		i++;
+	}
+	return (res);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ Code 0-memset.c fill a mememory area
 Code 1-memcpy.c copy n bytes of memory area
 Code 2-strchr.c locates a character in a string
 Code 3-strspn.c returns length of a prefix substring
+Code 4-strpbrk.c searches a string for any set of bytes
