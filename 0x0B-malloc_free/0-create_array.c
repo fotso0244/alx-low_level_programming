d409e768cc351336096db72e856d3062613eb4bf
aristide notouom <fotso0244@gmail.com>
2022-05-04 22:30:14 -0700

    How to create an array of char and initialize it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * create_array - creates an array of char and initialize it
+ * @size: size of array
+ * @c: character for initialization
+ *
+ * Return: pointer to char
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *res;
+	if (size == 0)
+		res = NULL;
+	else
+	{
+		res = malloc(sizeof(char) * size);
+		for (i = 0; i <= size - 1; i++)
+			res[i] = c;
+		res[i + 1] = '\0';
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Code 0-create_array.c creates an array of char and initialize it
