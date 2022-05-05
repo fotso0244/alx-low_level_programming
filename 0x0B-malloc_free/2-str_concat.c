c0a1d46f4aee963e319068b336ee950da5b24476
aristide notouom <fotso0244@gmail.com>
2022-05-05 14:40:24 -0700

    Update 2-str...

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -54,7 +54,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	if ((s1 != NULL) && (s2 != NULL))
 	{
-		res = malloc((sizeof(char) * (strlen(s1) + strlen(s2))));
+		res = malloc((sizeof(char) * (strlen(s1) + strlen(s2))) + 1);
 		if (res != NULL)
 		{
 			for (i = 0; i <= (int)strlen(s1) - 1; i++)
