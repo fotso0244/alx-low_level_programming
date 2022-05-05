ae8b4011ef54917b6b379ccce927bd6f8ea1e5ed
aristide notouom <fotso0244@gmail.com>
2022-05-04 20:35:08 -0700

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,14 @@
 #include <string.h>
+char *result(int j, int i, char *s)
+{
+	char *res;
+
+	if (j == (int)strlen(s))
+		res = s + (i - j + 1);
+	else
+		res = NULL;
+	return (res);
+}
 /**
  * _strstr - locates a substring
  * @haystack: string to scan
@@ -11,39 +21,30 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0, j = 0, k = 0;
 	char *res;
 
-	if (strcmp(needle, "") != 0)
+	while (*(haystack + i) != '\0')
 	{
-		while (*(haystack + i) != '\0')
+		if (*(haystack + i) == *(needle + j))
 		{
-			if (*(haystack + i) == *(needle + j))
+			k++;
+			j++;
+		}
+		else
+		{
+			if (haystack[i] != haystack [i - 1])
 			{
-				k++;
-				j++;
+				k = 0;
+				j = 0;
 			}
 			else
-			{
-				if (haystack[i] != haystack [i - 1])
+				if (k > 1)
 				{
 					k = 0;
 					j = 0;
 				}
-				else
-					if (k > 1)
-					{
-						k = 0;
-						j = 0;
-					}
-			}
-			if (k == (int)strlen(needle))
-				break;
-			i++;
 		}
 		if (k == (int)strlen(needle))
-			res = haystack + (i - k + 1);
-		else
-			res = NULL;
+			break;
+		i++;
 	}
-	else
-		res = NULL;
-	return (res);
+	return (result(k, i, needle));
 }
