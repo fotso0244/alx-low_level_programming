9291b9bee921b00f850402dac37bff797ccc013f
aristide notouom <fotso0244@gmail.com>
2022-05-05 10:09:03 -0700

    How to concat 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,29 @@
 #include <stddef.h>
 #include <string.h>
 /**
+ * str_concat2: concatenates 2 strings, 1 string is null
+ * @s1: first string
+ * @snull: second string
+ *
+ * Return: pointer to char
+ */
+char *str_concat2(char *s1, char *snull)
+{
+	int i;
+	char *res;
+
+	res = malloc(sizeof(char) * strlen(s1));
+	if (res != NULL)
+	{
+		for (i = 0; i <= (int)strlen(s1) - 1; i++)
+			res[i] = s1[i];
+		res[i + 1] = '\0';
+	}
+	else
+		res = NULL;
+	return (res);
+}
+/**
  * str_concat: concatenates 2 strings
  * @s1: first string
  * @s2: second string
@@ -15,27 +38,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		res = malloc(sizeof(char) * strlen(s2));
-		if (res != NULL)
-		{
-			for (i = 0; i <= (int)strlen(s2) - 1; i++)
-				res[i] = s2[i];
-			res[i + 1] = '\0';
-		}
-		else
-			res = NULL;
+		res = str_concat2(s2, s1);
 	}
 	if (s2 == NULL)
 	{
-		res = malloc(sizeof(char) * strlen(s1));
-		if (res != NULL)
-		{
-			for (i = 0; i <= (int)strlen(s1) - 1; i++)
-				res[i] = s1[i];
-			res[i + 1] = '\0';
-		}
-		else
-			res = NULL;
+		res = str_concat2(s1, s2);
 	}
 	if (s1 == NULL && s2 == NULL)
 		res = "";
