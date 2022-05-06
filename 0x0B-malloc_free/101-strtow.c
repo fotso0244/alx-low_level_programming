52178a3620033923edf0cc2333d45ee633bdcae3
aristide notouom <fotso0244@gmail.com>
2022-05-05 17:33:15 -0700

    How to split a string in words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,89 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * countwords - counts words
+ * @str: a string
+ *
+ * Return: number of words
+ */
+int countwords(char *str)
+{
+	int res = 0, i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!(str[i] <= 32 && str[i] >= 0))
+			i++;
+		else
+			if (str[i] == 32 && i != 0 && str[i - 1] > 32)
+			{
+				res++;
+				i++;
+			}
+			else
+				i++;
+	}
+	return (res);
+}
+/**
+ * countchar - counts character of a word
+ * @s: word
+ *
+ * Return: number of characters
+ */
+int countchar(char *s)
+{
+	int res = 0, i = 0;
+
+	while (s[i] > 32)
+	{
+		res++;
+		i++;
+	}
+	return (res);
+}
+/**
+ * strtow - splits a string in words
+ * @str: string
+ *
+ * Return: double pointer to int
+ */
+char **strtow(char *str)
+{
+	int i, j = 0, k = 0;
+	char **res;
+
+	if (str == NULL || strcmp(str, "") == 0)
+		res = NULL;
+	else
+	{
+		res = malloc(sizeof(char *) * (countwords(str) + 1));
+		if (res != NULL)
+		{
+			for (i = 0; i <= countwords(str); i++)
+			{
+				while (str[k] <= 32 && str[k] != '\0')
+					k++;
+				if (str[k] != '\0')
+				{
+					*(res + i) = malloc(sizeof(char) * (countchar(str + k) + 1));
+					if (res != NULL)
+					{
+						for (j = 0; j <= countchar(str + k) - 1; j++)
+							*(*(res + i) + j) = str[k + j];
+						*(*(res + i) + j) = '\0';
+						k += (j + 1);
+						j = 0;
+					}
+					else
+						res = NULL;
+				}
+			}
+			*(res + i + 1) = NULL;
+		}
+		else
+			res = NULL;
+	}
+	return (res);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,4 @@ Code 1-strdup.c returns pointer to duplicate string
 Code 2-str_concat.c concatenates 2 strings
 Code 3-alloc_grid.c  creates 2 dimensional array
 Code 4-free_grid.c frees 2-D array
+Code 101-strtow splits a string into words
