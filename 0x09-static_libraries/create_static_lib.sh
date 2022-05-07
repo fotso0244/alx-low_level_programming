2ff9924a87674a97213e07e0e73a972b97d035af
aristide notouom <fotso0244@gmail.com>
2022-05-07 00:45:44 -0700

    How to create static library with all .c files of current directory

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 for f in *.c
 do
-	oldstr=".c"
-	newstr=".o"
-	gcc -Wall -Werror -Wextra -pedantic -std=gnu89 $(echo $f) -o $(echo $f | sed "s/$oldstr/$newstr")
+	echo ${f/.c/.o}
+	gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c $(echo $f) -o $(echo ${f/.c/.o})
 done
 ar -rc liball.a $(echo *.o)
 ranlib liball.a
