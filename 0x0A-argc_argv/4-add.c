bd987c13a4a6a16473f59a7cbb48815122af844d
aristide notouom <fotso0244@gmail.com>
2022-05-07 15:02:55 -0700

    How to add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: list of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, res = 0;
+
+	if (argc != 1)
+	{
+		while (i <= argc - 1)
+		{
+			if (atoi(argv[i]) != 0)
+			{
+				res += atoi(argv[i]);
+				i++;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+				break;
+			}
+		}
+		printf("%d\n", res);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,4 @@ Code 0-whatsmyname.c prints name of program
 Code 1-args.c prints number of arguments passed to a program
 Code 2-args.c prints all arguments of a program
 Code 3-mul.c multiplies 2 numbers
+Code 4-add.c adds positive numbers
