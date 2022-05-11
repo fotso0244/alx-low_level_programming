388440a5c584f900a1a3521d87fe52e3498fa1ff
aristide notouom <fotso0244@gmail.com>
2022-05-10 21:31:22 -0700

    How to creates new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * new_dog - creates new object of struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: a pointer to dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *res;
+
+	res = malloc(sizeof(dog_t));
+	if (res != NULL)
+	{
+		res->name = malloc(strlen(name) + 1);
+		if (res->name != NULL)
+		{
+			strcpy(res->name, name);
+			res->name[strlen(name)] = '\0';
+		}
+		else
+			return (NULL);
+		res->owner = malloc(strlen(owner) + 1);
+		if (res->owner != NULL)
+		{
+			strcpy(res->owner, owner);
+			res->owner[strlen(owner)] = '\0';
+		}
+		else
+			return (NULL);
+		res->age = age;
+	}
+	else
+		return (NULL);
+	return (res);
+}
