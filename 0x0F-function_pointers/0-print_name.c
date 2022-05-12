7528c871b8a11ec06d8641e0b8f3f7d7f7e3e1f9
aristide notouom <fotso0244@gmail.com>
2022-05-12 07:59:44 -0700

    How to print name using function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include "function_pointers.h"
+#include <string.h>
 /**
  * print_name - prints name
  * @name: name to print
@@ -8,4 +10,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL)
 		(*f)(name);
+	if (name[(int)strlen(name) - 1] != '\n')
+		_putchar('\n');
 }
