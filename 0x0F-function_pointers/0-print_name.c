5aab4e93f977ec27d6c00a3d6197ece58e4673da
aristide notouom <fotso0244@gmail.com>
2022-05-11 21:15:33 -0700

    How to print a name using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: function to use
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Code 0-print_name.c prints name using pointer to function
