ac5fa81dea8bc1fe583dc926bd6e13e2be85e9dd
aristide notouom <fotso0244@gmail.com>
2022-05-12 20:24:47 -0700

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,27 +10,28 @@
  */
 int main(int argc, char *argv[])
 {
+	int (*f)(int, int) = get_op_func(argv[2]);
 	if (argc - 1 ==  3)
 	{
 		if (get_op_func(argv[2]) != NULL)
-			if ((get_op_func(argv[2]) == op_div || get_op_func(argv[2]) == op_mod) && argv[3] == 0)
+			if ((f == op_div || f == op_mod) && argv[3] == 0)
 			{
-				printf("Error\n");
+				printf("Error100\n");
 				exit(100);
 			}
 			else
 			{
-				printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+				printf("%d\n", (*f)(atoi(argv[1]), atoi(argv[3])));
 			}
 		else
 		{
-			printf("Error\n");
+			printf("Error99\n");
 			exit(99);
 		}
 	}
 	else
 	{
-		printf("Error\n");
+		printf("Error98\n");
 		exit(98);
 	}
 	return (0);
