6ad987fdb7459e996f9056ddbb37f5a3a1d62c76
aristide notouom <fotso0244@gmail.com>
2022-05-15 14:18:22 -0700

    How to multiplies 2 numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+/**
+ * printstr - prints a string using _putchar
+ * @s: a string
+ */
+void printstr(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	printstr(s + 1);
+}
+/**
+ * puiss10 - computes 10 power x
+ * @power: power
+ *
+ * Return: an unsigned int
+ */
+unsigned int puiss10(unsigned int power)
+{
+	unsigned int i = 1, res = 1;
+
+	while (i <= power)
+	{
+		res *= 10;
+		i++;
+	}
+	return (res);
+}
+/**
+ * tostring - converts positive number to string
+ * @mul: positive number
+ *
+ * Return: a pointer to string
+ */
+char *tostring(unsigned int mul)
+{
+	unsigned int i = 1, div = mul, j, mod;
+	char *s;
+
+	while (mul / puiss10(i) != 0)
+	{
+		i++;
+	}
+	s = malloc(sizeof(*s) * (i + 1));
+	if (s != NULL)
+	{
+		for (j = 0; j <= i - 1; j++)
+		{
+			mod = div % 10;
+			s[j] = mod + 48;
+			div /= 10;
+
+		}
+		s[j] = '\0';
+	}
+	return (s);
+}
+/**
+ * revstr - reverses a string
+ * @s: a string
+ */
+void revstr(char *s)
+{
+	int i, length, temp;
+
+	length = strlen(s);
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
+/**
+ * main - multiplies 2 positive numbers
+ * @argc: number of arguments
+ * @argv: list of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	unsigned int mul;
+	char *res;
+
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strcmp(argv[1], "0") != 0 && atoi(argv[1]) == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strcmp(argv[2], "0") != 0 && atoi(argv[2]) == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	mul = (unsigned int)atoi(argv[1]) * (unsigned int)atoi(argv[2]);
+	res = tostring(mul);
+	revstr(res);
+	printstr(res);
+	return (0);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -3,3 +3,4 @@ Code 0-malloc_checked.c allocates memory
 Code 2-calloc.c allocates memory and set it to zero
 Code 3-array_range.c creates an array of integer
 Code 100-realloc.c resizes allocated memory
+Code 101-mul.c multiplies 2 numbers
