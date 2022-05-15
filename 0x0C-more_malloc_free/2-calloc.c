f5e572e8382741319f9ca0f12672cf0c785191d3
aristide notouom <fotso0244@gmail.com>
2022-05-15 02:30:31 -0700

    How to allocate memory and set it to zero

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * calloc - allocates memory
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *res;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (nmemb != 0 && size != 0)
+	{
+		res = malloc(nmemb * size);
+		if (res != NULL)
+			memset(res, 0, nmemb * size);
+	}
+	return (res);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
 Code 1-string_nconcat concatenates 2 string
 Code 0-malloc_checked.c allocates memory
+Code 2-calloc.c allocates memory and set it to zero
