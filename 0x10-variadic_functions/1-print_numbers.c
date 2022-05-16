5c1689915d7090a4d5978ba3f781033ec9dbd0c3
aristide notouom <fotso0244@gmail.com>
2022-05-16 13:38:51 -0700

    How to print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: separator
+ * @n: number of integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n != 0)
+	{
+		va_start(ap, n);
+		for (i = 0; i <= n - 1; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (separator != NULL && i != n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+	}
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,2 @@
 Code 0-sum_them_all.c sums all arguments
+Code 1-print_numbers.c prints numbers
