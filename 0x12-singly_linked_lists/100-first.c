0523046ab047927a79ff55ef194cd7e5148e76cb
aristide notouom <fotso0244@gmail.com>
2022-05-24 08:44:21 -0700

    How to exceute a function before a main executed function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * beforeMain - function that executes before main
+ * beforeMain - function that executes
  */
+void beforeMain(void);
 __attribute__ ((constructor)) void beforeMain(void)
 {
 	printf("You're beat! and yet, you must allow,");
