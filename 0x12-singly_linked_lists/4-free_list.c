92f9d2fc426baea6b516a9e6d1bb63d69a67d062
aristide notouom <fotso0244@gmail.com>
2022-05-24 05:44:49 -0700

    How to free a list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,13 +2,13 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- *  * free_list - frees a list_t list
- *   * @head: a list_t list
- *    */
+ * free_list - frees a list_t list
+ * @head: a list_t list
+ */
 void free_list(list_t *head)
 {
 	list_t *c;
-	
+
 	if (head != NULL)
 	{
 		while (head != NULL)
