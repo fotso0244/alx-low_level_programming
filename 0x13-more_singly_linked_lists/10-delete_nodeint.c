32170bc1a233b3f1f262aa792dd8a333793e22ff
aristide notouom <fotso0244@gmail.com>
2022-05-25 04:49:58 -0700

    How to delete a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,7 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *c = *head, *prec;
 	int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || index >= listint_len(*head))
 		return (-1);
 	if (index != 0 && index <= listint_len(*head) - 1)
 	{
