bc2b93da63b00516bb3ea15b90513b14c219f973
aristide notouom <fotso0244@gmail.com>
2022-05-24 20:36:33 -0700

    How to insert a new node at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - inserts a new node
+ * @head: a listint_t list
+ * @index: index of the new node
+ * @n: data n of new node
+ *
+ * Return: the new listint_t list, otherwise NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+	listint_t *c = NULL, *new, *prec;
+	unsigned int i = 0;
+
+	if (*head != NULL)
+	{
+		c = *head;
+		while (i < index && c != NULL)
+		{
+			prec = c;
+			c = c->next;
+			i++;
+		}
+		new = malloc(sizeof(listint_t));
+		if (new != NULL && c != NULL)
+		{
+			new->n = n;
+			prec->next = new;
+			if (index != 0)
+				new->next = c;
+			else
+				new->next = NULL;
+		}
+		else
+			if (c == NULL)
+				return (NULL);
+	}
+	return (c);
+}
