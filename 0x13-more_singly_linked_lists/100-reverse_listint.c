dde846567d9200f8369bf20b5d5c088521cae39c
aristide notouom <fotso0244@gmail.com>
2022-05-25 21:20:03 -0700

    How to reverse a listint_t list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *cur = *head, *prev = NULL;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	while (cur->next != NULL)
 	{
