8e04462569d95e60ab7ca8b040acd7e8968ab31b
aristide notouom <fotso0244@gmail.com>
2022-05-28 08:12:26 -0700

    How to convert a binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,9 +7,9 @@
  *
  * Return: 2 power i
  */
-unsigned int puiss2(unsigned int i)
+int puiss2(int i)
 {
-	unsigned int res = 1, j = 0;
+	int res = 1, j = 0;
 
 	while (j <= i - 1)
 	{
@@ -24,14 +24,20 @@ unsigned int puiss2(unsigned int i)
  *
  * Return: an unsigned int
  */
-unsigned int binary_to_int(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = (unsigned int)strlen(b) - 1, res = 0;
+	int i = (int)strlen(b) - 1, res = 0, j = 0;
 
 	while (i >= 0)
 	{
-		res += ((unsigned int)b[i] - 48) * puiss2(i);
-		i--;
+		if (b[i] == 48 || b[i] == 49)
+		{
+			res += ((b[j] - 48) * puiss2(i));
+			i--;
+			j++;
+		}
+		else
+			return (0);
 	}
-	return (res);
+	return ((unsigned int)res);
 }
