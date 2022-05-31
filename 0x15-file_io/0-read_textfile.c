157b41625ae3db384c82a907f5869c34eb0de019
aristide notouom <fotso0244@gmail.com>
2022-05-31 05:17:00 -0700

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,35 @@
 #include <fcntl.h>
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * count_char - returns number of character in a file
+ * @filename: a text file
+ *
+ * Return: number of characters
+ */
+size_t count_char(const char *filename)
+{
+	size_t i = 0;
+	int fd;
+	char c;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd != -1)
+		{
+			read(fd, &c, 1);
+			while (c != EOF)
+			{
+				i++;
+				read(fd, &c, 1);
+			}
+		}
+	}
+	close(fd);
+	return (i);
+}
 /**
  * read_textfile - read a text file
  * @filename: a file name
@@ -13,30 +42,26 @@
 size_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, r;
-	size_t w;
+	size_t w = 0;
 	char *buf;
 
-	if (filename != NULL)
+	if (filename != NULL && letters != 0)
 	{
 		fd = open(filename, O_RDONLY);
 		if (fd != -1)
 		{
-			buf = malloc(sizeof(*buf) * letters);
-			if (buf != NULL)
+			if (count_char(filename) != 0)
 			{
-				r = read(fd, buf, letters);
-				if (r != -1)
-					w = write(STDOUT_FILENO, buf, r);
-				else
-					w = 0;
+				buf = malloc(sizeof(*buf) * count_char(filename));
+				if (buf != NULL)
+				{
+					r = read(fd, buf, letters);
+					if (r != -1)
+						w = write(STDOUT_FILENO, buf, r);
+				}
 			}
-			else
-				w = 0;
 		}
-		else
-			w = 0;
 	}
-	else
-		w = 0;
+	close(fd);
 	return (w);
 }
