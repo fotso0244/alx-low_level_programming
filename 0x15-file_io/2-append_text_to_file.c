59a7d7033b0415e9e58763961a1b18860f614951
aristide notouom <fotso0244@gmail.com>
2022-05-31 09:02:42 -0700

    How to append a text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		fd = open(filename, O_WRONLY | O_APPEND);
 		if (fd != -1)
 		{
-			if (strcmp(text_content, "NULL") != 0)
+			if (text_content != NULL)
 			{
 				write(fd, text_content, (int)strlen(text_content));
 				w = 1;
