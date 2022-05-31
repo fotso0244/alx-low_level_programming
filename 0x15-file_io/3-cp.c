9111c5612f33d61794be0ea115b16d9b6538b5fb
aristide notouom <fotso0244@gmail.com>
2022-05-31 10:19:56 -0700

    How to copy a file text

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,143 @@
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * append_text_to_file - appends text at end of a file
+ * @filename: a file name
+ * @text_content: a text to append to a file
+ *
+ * Return: 1 if success, otherwise -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w = -1;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd != -1)
+		{
+			if (text_content != NULL)
+			{
+				write(fd, text_content, (int)strlen(text_content));
+				w = 1;
+			}
+			else
+				w = 1;
+			close(fd);
+		}
+	}
+	return (w);
+}
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w = -1;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (fd != -1)
+		{
+			if (text_content != NULL)
+			{
+				w = write(fd, text_content, (int)strlen(text_content));
+				if (w != -1)
+					w = 1;
+				else
+					w = -1;
+			}
+			close(fd);
+		}
+	}
+	return (w);
+}
+size_t count_char(const char *filename)
+{
+	size_t i = 0;
+	int fd;
+	char *c;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd != -1)
+		{
+			c = malloc(sizeof(char));
+			read(fd, c, 1);
+			while (strcmp(c, "") != 0)
+			{
+				i++;
+				free(c);
+				c = malloc(sizeof(char));
+				read(fd, c, 1);
+			}
+			close(fd);
+		}
+	}
+	free(c);
+	return (i);
+}
+/**
+ * main - copies a file
+ * @argc: number of arguments
+ * @argv: list of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int fd, count, c, cl, choice = 0;
+	char *buf;
+	
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	count = count_char(argv[1]);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (fd != -1)
+	{
+		if (count != 0)
+		{
+			while (count >= 0)
+			{
+				buf = malloc(sizeof(*buf) * 1024);
+				if (buf != NULL)
+				{
+					read(fd, buf, 1024);
+					if (choice == 0)
+					{
+						c = create_file(argv[2], buf);
+						choice = 1;
+					}
+					else
+						c = append_text_to_file(argv[2], buf);
+					free(buf);
+					count -= 1024;
+					if (c == -1)
+					{
+						dprintf(2, "Error: Can't write to %s\n", argv[2]);
+						exit(99);
+					}
+				}
+			}
+		}
+		cl = close(fd);
+		if (cl == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", fd);
+			exit(100);
+		}
+	}
+	return (0);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,3 +1,4 @@
 Code 0-read_textfile.c reads a text file and print it to stdout
 Code 1-create_file.c creates a file with 600 set permission
 Code 2-append_text_to_file.c append a text to a file
+3-cp.c copies a file
