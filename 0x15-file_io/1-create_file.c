15b1077a4ae4821757b6b9b9ad3a4e387583b6f8
aristide notouom <fotso0244@gmail.com>
2022-05-31 07:43:05 -0700

    How to create a file with set of permissions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * create_file - creates a text file
+ * @filename: a file name
+ * text_content: a text to fill a file
+ *
+ * Return: 1 if success, otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w = -1;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_EXCL, 0600);
+		if (fd != -1)
+		{
+			if (text_content != NULL)
+			{
+				w = write(fd, text_content, (int)strlen(text_content));
+				if (w != -1)
+					w = 1;
+				else
+					w = -1;
+			}
+			close(fd);
+		}
+	}
+	return (w);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,1 +1,2 @@
 Code 0-read_textfile.c reads a text file and print it to stdout
+Code 1-create_file.c creates a file with 600 set permission
