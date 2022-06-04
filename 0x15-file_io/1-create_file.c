552cdb8ba7746b964da0d0559a7a5e0aedf0a31b
aristide notouom <fotso0244@gmail.com>
2022-06-03 21:25:39 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,8 @@ int create_file(const char *filename, char *text_content)
 	if (strcmp(filename, "NULL") != 0 && strcmp(filename, "") != 0)
 	{
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (strcmp(text_content, "") == NULL)
+			return (1);
 		if (fd != -1)
 		{
 			if (text_content != NULL)
