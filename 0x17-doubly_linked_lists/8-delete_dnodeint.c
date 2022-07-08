0beab251aa72196aa05615468f7a7a557ded161c
aristide <fotso0244@gmail.com>
2022-07-08 18:33:18 +0000

    How to delete a node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -51,6 +51,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			upcurr = curr->next;
 			curr->next = upcurr->next;
+			upcurr->next->prev = curr;
 			free(upcurr);
 			return (1);
 		}
