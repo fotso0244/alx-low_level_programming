a3e619ad28e04a7d20bc936ac659cfa5334ec629
aristide <fotso0244@gmail.com>
2022-07-08 08:31:58 +0000

    How to add new element at end of a doubly list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add_dnodeint_end - adds a new element at the end
+ * @head: a doubly linked list
+ * @n: value
+ *
+ * Return: address to new element
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *curr = *head;
+
+	new = malloc(sizeof(*new));
+	if (new != NULL)
+	{
+		new->n = n;
+		new->prev = NULL;
+	}
+	if (*head == NULL || head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
+	else
+	{
+		while (curr->next != NULL)
+			curr = curr->next;
+	}
+	new->next = NULL;
+	curr->next = new;
+	new->prev = curr;
+	curr = curr->next;
+	return (curr);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,3 +1,4 @@
 Code 0-print_dlistint prints all elements of a double list
 Code 1-dlistint_t returns number of elements
 Code 2-add_nodeint adds a new element at beginning
+Code 3-add_dnodeint_end adds a new element at end of a doubly list
